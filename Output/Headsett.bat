aa9a020e92e2591e345292513d1c65d3f6253788
Jan Greger Hemb <Jan Greger Hemb>
2016-07-24 01:02:26 +0200

    Switching between headset and speakers

---
+++ Output/Headsett.bat
@@ -0,0 +1,4 @@
+nircmd.exe setdefaultsounddevice "G930" 1
+nircmd.exe setdefaultsounddevice "G930" 2
+//nircmd.exe speak text "Headset"
+exit
---
+++ Output/Speakers.bat
@@ -0,0 +1,4 @@
+nircmd.exe setdefaultsounddevice "Speakers" 1
+nircmd.exe setdefaultsounddevice "Speakers" 2
+//nircmd.exe speak text "Speakers"
+exit
